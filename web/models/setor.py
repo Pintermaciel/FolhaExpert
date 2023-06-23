2a0133c0da721a596b7c9be7ab71d97802eefe16
Matheus Henrique Pinter Maciel <pintermaciel@gmail.com>
2023-06-23 16:04:00 -0300

    Update setor.py

--- web/models/setor.py
+++ web/models/setor.py
@@ -2,6 +2,12 @@ import sqlite3
 import traceback
 
 def showallrecords():
+    """
+    Retorna todos os registros da tabela 'setor' ordenados por ID.
+
+    Returns:
+        list: Lista contendo os registros da tabela 'setor'.
+    """
     try:
         connect = sqlite3.connect(r"web/databases/storage.db")
         cursor = connect.cursor()
@@ -16,11 +22,23 @@ def showallrecords():
         return msg
 
 def save_newsetor(empresa, setor, funcao, lider):
+    """
+    Insere um novo registro de setor no banco de dados.
+
+    Args:
+        empresa (str): Nome da empresa.
+        setor (str): Nome do setor.
+        funcao (str): Função do setor.
+        lider (str): Líder do setor.
+
+    Returns:
+        str: Mensagem de sucesso ou falha.
+    """
     try:
         connect = sqlite3.connect(r"web/databases/storage.db")
         cursor = connect.cursor()
         print("Conexão com o banco de dados estabelecida com sucesso!")
-        
+
         if empresa != "" and setor != "" and funcao != "" and lider != "":
             cursor.execute("INSERT INTO setor(empresa, setor, funcao, lider) VALUES(?,?,?,?)", (empresa,setor,funcao,lider))
             connect.commit()
@@ -37,18 +55,18 @@ def save_newsetor(empresa, setor, funcao, lider):
     
 def show_selectedSetor(id):
     """
-    Retorna um registro de admissão específico com base no ID fornecido.
+    Retorna um registro de setor específico com base no ID fornecido.
 
     Args:
-        id (int): ID do registro de admissão.
+        id (int): ID do registro de setor.
 
     Returns:
-        list: Lista contendo os detalhes do registro de admissão.
+        list: Lista contendo os detalhes do registro de setor.
     """
     try:
         connect = sqlite3.connect("web/databases/storage.db")
         cursor = connect.cursor()
-        print("Conexão com o banco de dados estabelecida com sucesso! show_selectedAdmissao")
+        print("Conexão com o banco de dados estabelecida com sucesso! show_selectedSetor")
         cursor.execute("SELECT * FROM setor WHERE id =?", (id,))
         editadmissao = []
         for item in cursor.fetchone():
@@ -62,14 +80,14 @@ def show_selectedSetor(id):
     
 def update_setor(empresa, setor, funcao, lider, id):
     """
-    Atualiza um registro de admissão existente no banco de dados.
+    Atualiza um registro de setor existente no banco de dados.
 
     Args:
         empresa (str): Novo nome da empresa.
         setor (str): Novo setor do funcionário.
         funcao (str): Nova função do funcionário.
         lider (str): Novo líder do setor.
-        id (int): ID do registro de admissão a ser atualizado.
+        id (int): ID do registro de setor a ser atualizado.
 
     Returns:
         str: Mensagem de sucesso ou falha.
